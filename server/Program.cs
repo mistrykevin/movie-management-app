ef76ab4184e6a9aa003022e43214b8990e3300fa
Kevin <155801407+mistrykevin@users.noreply.github.com>
2025-08-05 12:00:05 +0530

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -3,14 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+
 // Add DB context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
                      new MySqlServerVersion(new Version(8, 0, 0))));
 
-builder.Services.AddCors();
-builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthorization();
